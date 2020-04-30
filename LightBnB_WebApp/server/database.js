aff2526793b81cbcec668183b46c5d695253f02f
Sean Sullivan <dev@seanmedia.ca>
2020-04-30 20:57:07 +0000

    Rewrite addProperty to insert new property into database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -159,7 +159,7 @@ const getAllProperties = function(options, limit = 10) {
     ORDER BY cost_per_night
     LIMIT $${queryParams.length};
   `;
-  
+
   return pool
     .query(queryString, [limit])
     .then(res => res.rows);
@@ -173,9 +173,30 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, street, city, province, country, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+  const values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.street,
+    property.city,
+    property.province,
+    property.country,
+    property.post_code
+  ];
+
+  return pool
+    .query(queryString, values)
+    .then(res => res.rows.length ? res.rows[0] : null);
 };
 exports.addProperty = addProperty;
